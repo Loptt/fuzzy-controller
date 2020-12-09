ed9e0a934ceb00956b19d7974127a8889bed050b
Estrada Guerra <ce20959@LX-VLAB1-005.essex.ac.uk>
2020-12-09 10:25:24 +0000

    Added tuned wall following

--- wall_following.py
+++ wall_following.py
@@ -1,6 +1,6 @@
-import fuzzy_set as fs
-import fuzzy_value as fv
-import fuzzy_controller as fc
+from fuzzy_controller import fuzzy_set as fs
+from fuzzy_controller import fuzzy_value as fv
+from fuzzy_controller import fuzzy_controller as fc
 
 
 # Controller to know how much right to turn
@@ -13,9 +13,9 @@ left_controller = None
 def setup():
     global right_controller, left_controller
     wall_value = fv.FuzzyValue()
-    wall_value.add_set(fs.FuzzySet("Close", 0, 0.1, 0.4, 0.5))
-    wall_value.add_set(fs.FuzzySet("Medium", 0.4, 0.5, 0.5, 0.6))
-    wall_value.add_set(fs.FuzzySet("Far", 0.5, 0.6, 0.9, 1))
+    wall_value.add_set(fs.FuzzySet("Close", 0, 0, 0.2, 0.5))
+    wall_value.add_set(fs.FuzzySet("Medium", 0.2, 0.5, 0.5, 0.8))
+    wall_value.add_set(fs.FuzzySet("Far", 0.5, 0.8, 1.2, 11))
 
     front_value = fv.FuzzyValue()
     front_value.add_set(fs.FuzzySet("Close", 0, 0.2, 0.3, 0.5))
@@ -28,9 +28,9 @@ def setup():
     speed_value.add_set(fs.FuzzySet("High", 0.4, 0.5, 0.6, 1))
 
     turn_value = fv.FuzzyValue(output=True)
-    turn_value.add_set(fs.FuzzySet("Centered", 0, 0, 0.05, 0.1))
-    turn_value.add_set(fs.FuzzySet("Low Turn", 0.5, 1, 1.5, 2))
-    turn_value.add_set(fs.FuzzySet("High Turn", 1.5, 2, 4, 4))
+    turn_value.add_set(fs.FuzzySet("Centered", 0, 0, 0.01, 0.05))
+    turn_value.add_set(fs.FuzzySet("Low Turn", 0.01, 0.05, 0.05, 0.1))
+    turn_value.add_set(fs.FuzzySet("High Turn", 0.1, 0.5, 1, 1.4))
 
     right_controller = fc.FuzzyController(
         [wall_value, front_value], [speed_value, turn_value], fc.FuzzyController.FuzzyOperator.AND)
@@ -62,8 +62,6 @@ def calculate_turn(right_distance, front_distance):
     right_turn = right_controller.fire([right_distance, front_distance])[1]
     left_turn = left_controller.fire([right_distance, front_distance])[1]
     
-    return right_turn - left_turn
+    return left_turn - right_turn
 
 
-setup()
-print(calculate_turn(0.2, 2))
